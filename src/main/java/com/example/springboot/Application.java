7935e627be40c3498dc317cfa708b465f0299c16
Wastard <Ilya.pakylov@gmail.com>
2024-01-02 18:58:02 +0300

    added artist service; set artist entity

--- src/main/java/com/example/springboot/Application.java
+++ src/main/java/com/example/springboot/Application.java
@@ -2,12 +2,17 @@ package com.example.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.repository.CrudRepository;
+
+import com.example.springboot.db.entities.Artist;
+import com.example.springboot.db.services.ArtistService;
+
 
 @SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		Artist artist = new Artist("Сальвадор", "Дали");
 	}
-
 }
