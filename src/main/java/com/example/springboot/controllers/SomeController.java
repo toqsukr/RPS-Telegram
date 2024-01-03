91335626bb962e5159913be08dd93f17535c2716
Wastard <Ilya.pakylov@gmail.com>
2024-01-03 15:11:01 +0300

    successful connection; CORS issue again -_-

--- src/main/java/com/example/springboot/controllers/SomeController.java
+++ src/main/java/com/example/springboot/controllers/SomeController.java
@@ -1,13 +1,14 @@
 package com.example.springboot.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.springboot.db.entities.Artist;
 import com.example.springboot.db.services.ArtistService;
 
-@Controller
+@RestController
+@RequestMapping("/")
 public class SomeController {
     private final ArtistService artistService;
 
