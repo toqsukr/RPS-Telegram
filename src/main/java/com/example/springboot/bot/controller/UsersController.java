701f8465ef6442245acdf492472d75ba5be5cd65
Wastard <Ilya.pakylov@gmail.com>
2024-01-04 19:08:52 +0300

    deleted useless classes; added controller & exception

--- src/main/java/com/example/springboot/bot/controller/UsersController.java
+++ src/main/java/com/example/springboot/bot/controller/UsersController.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.springboot.bot.dto.User;
+import com.example.springboot.bot.service.UserService;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
