fb60da019a8f88f228f1cd9aec6ef0d6e05712de
Wastard <Ilya.pakylov@gmail.com>
2024-01-09 18:43:17 +0300

    updated frontend urls

--- src/main/java/com/example/painting/bot/config/WebConfig.java
+++ src/main/java/com/example/painting/bot/config/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://rps-frontend-theta.vercel.app")
+                .allowedOrigins("https://rps-frontend-one.vercel.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*")
                 .allowCredentials(true);
--- src/main/java/com/example/painting/utils/Constants.java
+++ src/main/java/com/example/painting/utils/Constants.java
@@ -3,5 +3,5 @@ package com.example.painting.utils;
 public interface Constants {
     String START_MESSAGE = "Привет! Я бот, который увлекается живописью :D. Меня создали для того чтобы помочь тебе разобраться в своих вкусах в искусстве. Я буду рекомендовать тебе различные работы художников прошлых лет, а ты оценивай те понравившиеся, чтобы улучшить мои рекомендации. Договорились? Желаю приятного просмотра!";
     String START_DESCRIPTION = "Старт";
-    String TWA_URL = "https://rps-frontend-theta.vercel.app/";
+    String TWA_URL = "https://rps-frontend-one.vercel.app";
 }
